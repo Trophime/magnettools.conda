d141d64576d81f67d068b2f62ce4fc4c51fea861
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2022-01-26 08:51:48 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# Build MagnetTools
+# pwd
+# echo "PREFIX=${PREFIX}"
+# ls
+# cd magnettools
+
+echo "PREFIX=${PREFIX}"
+
+mkdir -p build
+cd build
+cmake .. \
+      -DMAGNETTOOLS_ENABLE_OPTIONALDEPS=ON \
+      -DMAGNETTOOLS_ENABLE_PYTHON=ON \
+      -DMAGNETTOOLS_PYTHON_VERSION=3
+ 
+
+make DESTDIR=${PREFIX}
+make DESTDIR=${PREFIX} install
+
+# # Build MagnetTools Python wrapper
+# cd ../Python
+# swig -python -shadow -c++ MagnetType.i
+# python setup.py build
+# python setup.py install --prefix=$PREFIX
